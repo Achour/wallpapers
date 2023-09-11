1f0afdf073e4ace9a4f3082069e894c15d0a72b9
Meguenni Achour <achourphp@gmail.com>
2023-09-11 18:39:28 +0100

    moved test/page.jsx to /page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-
+"use client"
 
 import WallpaperCard from '@/components/WallpaperCard'
 import Image from 'next/image'
@@ -7,21 +7,42 @@ import { createClient } from 'pexels';
 
 import { fetchImages } from '@/utils/pexels';
 import ShowMore from '@/components/ShowMore';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import Filters from '@/components/Filters';
 
+import { useState } from 'react';
 
-export default async function Home({ searchParams }) {
+export default function Home() {
+  const per_page = 10;
+  const [images, setimages] = useState()
+  const [more, setmore] = useState(10)
+  const [nature, setnature] = useState("dog")
+  const [loading, setloading] = useState(false)
 
-  const per_page = parseInt(searchParams.page || 10);
-  const nature = searchParams.query || "nature"
-  console.log(per_page)
-  const images = await fetchImages(nature, per_page);
+  useEffect(() => {
+    async function getData() {
+      const data = await fetchImages(nature, more)
+      setimages(data)
+    }
+    getData();
+    return () => {
+      setloading(false)
+
+    }
+  }, [nature, more])
+
+  function handleClick() {
+    setloading(true)
+    setmore(more + 10)
+  }
 
   return (
 
     <>
-      <Filters />
+      <div className='text-gray-900 mb-10 flex justify-end'>
+        <input onChange={(e) => setnature(e.target.value)} className='bg-slate-400 me-5 rounded-xl px-3 w-64 py-2' type="text" value={nature} />
+
+      </div>
 
       <div className='wallpapers_wrapper'>
         {images?.map((item) => (
@@ -33,7 +54,8 @@ export default async function Home({ searchParams }) {
 
       <div className='flex justify-center py-10'>
 
-        <ShowMore perPage={per_page} />
+
+        <button disabled={loading} onClick={handleClick} className='bg-slate-950 px-5 py-3 rounded-xl text-white font-bold'>Get More </button>
 
       </div>
 
