fcf03101482e25ebe67f3a5ec0a9f1c213efa30f
Meguenni Achour <achourphp@gmail.com>
2025-03-28 03:10:04 +0100

    refactor Home component grid to include an additional column for improved layout

--- app/page.jsx
+++ app/page.jsx
@@ -49,7 +49,7 @@ export default function Home() {
     <>
 
       <div className=' grid 
-    lg:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-5  '>
+    xl:grid-cols-5 lg:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-5  '>
         {images?.map((item, index) => (
 
           <WallpaperCard key={index} photoGrapher={item.photographer} avgColor={item.avg_color} wallpaperSrc={item.src.portrait} wallpaperOrg={item.src.portrait} />
