c4f26bce4d7c39acdaa64042893a39e2489044dc
Meguenni Achour <achourphp@gmail.com>
2025-03-28 02:49:37 +0100

    add theme provider and mode toggle components for theme management

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Button } from '@/components/ui/button';
 import { Github } from "lucide-react"
+import { ModeToggle } from '@/components/mode-toggle';
 
 export default function Navbar() {
     let [isOpen, setIsOpen] = useState(false)
@@ -18,18 +19,21 @@ export default function Navbar() {
 
         <header className='border-b h-16 flex items-center'>
             <div className="container">
-                <div className="flex gap-y-4 justify-between items-center">
-                    <ul className=' flex  gap-5'>
+                <div className="flex justify-between items-center">
+                    <ul className='flex  gap-4'>
                         <li><a href="/">Home</a></li>
                         <li><a target='_blank' href="https://mrdigitus.com/">Portfolio</a></li>
                         <li><a target='_blank' href="https://x.com/mr_digitus">Contact</a></li>
                     </ul>
-                    <Button asChild>
-                        <a target='_blank' href="https://github.com/Achour/">
-                            <Github />
-                            GitHub
-                        </a>
-                    </Button>
+                    <div className='flex gap-4'>
+                        <ModeToggle />
+                        <Button asChild>
+                            <a target='_blank' href="https://github.com/Achour/">
+                                <Github />
+                                GitHub
+                            </a>
+                        </Button>
+                    </div>
                 </div>
             </div>
         </header >
--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 import { Suspense } from "react";
 import Hero from "@/components/layout/hero";
+
+import { ThemeProvider } from "@/components/theme-provider";
+
 export const metadata = {
   title: "Wallpapers Web App",
   description: "Created by Achour Meguenni",
@@ -12,12 +15,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <Suspense>
-          <Navbar />
-          <Hero />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Suspense>
+            <Navbar />
+            <Hero />
 
-          <div className="container my-12">{children}</div>
-        </Suspense>
+            <div className="container my-12">{children}</div>
+          </Suspense>
+        </ThemeProvider>
       </body>
     </html>
   );
